a191c8c720caad5afb86fc59d091992ecc85afb7
ArneeMe <42463181+ArneeMe@users.noreply.github.com>
2018-10-12 14:36:40 +0200

    Ny update!

---
+++ Enkle prosjekter/Javascript_test.js
@@ -0,0 +1,11 @@
+console.log("test");
+
+//prompt("heihei")
+
+document.write("<p>hei<p>");
+
+document.getElementById("strand").src="https://images.pexels.com/photos/533923/pexels-photo-533923.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+
+document.body.style.backgroundColor= "blue";
+
+document.getElementById().width=150px
